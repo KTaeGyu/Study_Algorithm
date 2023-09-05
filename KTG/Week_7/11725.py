8c48b89d9a50237bc7aa2803e0eefff6c1f297ae
김태규 <xorb269@naver.com>
2023-09-06 08:58:32 +0900

    0906

--- KTG/Week_7/11725.py
+++ KTG/Week_7/11725.py
@@ -8,17 +8,34 @@
 import sys
 from pprint import pprint
 sys.stdin = open("../test.txt", "r")
+from collections import deque
 
 N = int(input())
-graph = {}
-for i in range(1, N+1):
-    graph[i] = []
-print(graph)
+
+graph = [[] for _ in range(N+1)]
 for i in range(N-1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 print(graph)
+
+tree = [0 for _ in range(N+1)]
+
+q = deque()
+q.append(1)
+v = {1}
+while q:
+    now = q.popleft()
+    for i in graph[now]:
+        if i not in v:
+            tree[i] = now
+            q.append(i)
+            v.add(i)
+
+for i in range(2, N+1):
+    print(tree[i])
+
+
 
 """
 7
